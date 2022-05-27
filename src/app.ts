0f455f796d14efd43deca4d0b787a514f7c4fb33
perterpaw <pb.pawelczyk@gmail.com>
2022-05-27 22:43:40 +0200

    Set up connection to db

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import express, { Request, Response } from "express"
 import mysql from "mysql"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
+
+const connectionString = process.env.DATABASE_URL || ""
+const connection = mysql.createConnection(connectionString)
+connection.connect()
+
+app.get("/api/table", (req: Request, res: Response) => {
+  const query = "SELECT * FROM ladder"
+  connection.query(query, (err, rows) => {
+    if (err) throw err
+
+    const retVal = {
+      data: rows,
+      message: rows.length === 0 ? "No records found" : null
+    }
+
+    return res.send(retVal)
+  })
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
