be05ea0a11768636cc14119ba809912fb60db437
peterpaw <pb.pawelczyk@gmail.com>
2022-05-29 09:32:58 +0200

    Add placing data to api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express"
 import mysql from "mysql"
 import dotenv from "dotenv"
 
+import { RowProps } from "./seed"
+
 dotenv.config()
 
 const app = express()
@@ -10,6 +12,17 @@ const connectionString = process.env.DATABASE_URL || ""
 const connection = mysql.createConnection(connectionString)
 connection.connect()
 
+// Find position of current team
+function getPosition(rows: RowProps[]): number {
+  const positionInArray = rows.findIndex((r: any) =>
+    r.team_name.match(/Rot-Weiss Walldorf/)
+  )
+
+  if (positionInArray !== -1) {
+    return positionInArray + 1
+  } else return positionInArray
+}
+
 app.get("/api/rww1", (req: Request, res: Response) => {
   const query = "SELECT * FROM team_one"
   connection.query(query, (err, rows) => {
@@ -17,6 +30,7 @@ app.get("/api/rww1", (req: Request, res: Response) => {
 
     const retVal = {
       data: rows,
+      placing: getPosition(rows),
       message: rows.length === 0 ? "No records found" : null
     }
 
@@ -31,6 +45,7 @@ app.get("/api/rww2", (req: Request, res: Response) => {
 
     const retVal = {
       data: rows,
+      placing: getPosition(rows),
       message: rows.length === 0 ? "No records found" : null
     }
 
@@ -45,6 +60,7 @@ app.get("/api/rww3", (req: Request, res: Response) => {
 
     const retVal = {
       data: rows,
+      placing: getPosition(rows),
       message: rows.length === 0 ? "No records found" : null
     }
 
--- src/seed.ts
+++ src/seed.ts
@@ -9,7 +9,7 @@ const connectionString = process.env.DATABASE_URL || ""
 const connection = mysql.createConnection(connectionString)
 connection.connect()
 
-interface RowProps {
+export interface RowProps {
   position: number
   teamName: string
   games: string
@@ -23,6 +23,7 @@ const getTeamPage = async (teamNumber: string, url: string) => {
   const table = $("table.result-set")
   const tRows = $(table[0]).find("tr")
 
+  // Build array of objects with desired data
   let leagueData: RowProps[] = []
 
   let eachIndex = 0
@@ -32,9 +33,10 @@ const getTeamPage = async (teamNumber: string, url: string) => {
       const current = $(this).find("a[alt='Mannschaftsportrait']")
       const scoreElement = $(current).parent().siblings().splice(6, 1)
       const gamesPlayed = $(current).parent().siblings().splice(2, 1)
-      // console.log(gamesPlayed.map((e) => $(e).text()))
+
       eachIndex += 1
       const internalIndex = 0
+
       leagueData.push({
         position: eachIndex,
         teamName: $(this).find("a").text(),
@@ -44,8 +46,6 @@ const getTeamPage = async (teamNumber: string, url: string) => {
     }
   })
 
-  console.log(leagueData)
-
   const values = leagueData.map((team) => [
     team.position,
     team.position,
@@ -53,34 +53,47 @@ const getTeamPage = async (teamNumber: string, url: string) => {
     team.games,
     team.points
   ])
-  const sql = `INSERT INTO team_${teamNumber} (id, position, team_name, games, points) VALUES ?`
-  connection.query(sql, [values], (err) => {
+
+  // 1.) delete old content
+  const deleteSql = `DELETE FROM team_${teamNumber}`
+  connection.query(deleteSql, function (err) {
     if (err) {
-      console.error("An error occured while populating database")
+      console.error("Error while deleting old content")
       console.error(err)
     } else {
-      console.log("Database population successful!")
+      console.log("Deleting old content successful!")
+
+      // 2.) Populate database with new content
+      const sql = `INSERT INTO team_${teamNumber} (id, position, team_name, games, points) VALUES ?`
+      connection.query(sql, [values], (err) => {
+        if (err) {
+          console.error("An error occured while populating database")
+          console.error(err)
+        } else {
+          console.log("Database population successful!")
+        }
+      })
     }
   })
 
-  // let teamIndex = -1
+  let teamIndex = -1
 
-  // leagueData.forEach((team, index) => {
-  //   const teamCheck = new RegExp(/Rot-Weiss Walldorf/)
-  //   if (teamCheck.test(team.teamName)) {
-  //     teamIndex = index
-  //   }
-  // })
+  leagueData.forEach((team, index) => {
+    const teamCheck = new RegExp(/Rot-Weiss Walldorf/)
+    if (teamCheck.test(team.teamName)) {
+      teamIndex = index
+    }
+  })
 
-  // console.log(
-  //   teamIndex !== -1
-  //     ? `${leagueData[teamIndex].teamName} befindet sich an Position ${
-  //         teamIndex + 1
-  //       }`
-  //     : "nicht in der Tabelle"
-  // )
+  console.log(
+    teamIndex !== -1
+      ? `${leagueData[teamIndex].teamName} befindet sich an Position ${
+          teamIndex + 1
+        }`
+      : "nicht in der Tabelle"
+  )
 }
 
-const team1 = getTeamPage("one", process.env.TEAM_1_URL || "")
-const team2 = getTeamPage("two", process.env.TEAM_2_URL || "")
-const team3 = getTeamPage("three", process.env.TEAM_3_URL || "")
+getTeamPage("one", process.env.TEAM_1_URL || "")
+getTeamPage("two", process.env.TEAM_2_URL || "")
+getTeamPage("three", process.env.TEAM_3_URL || "")
